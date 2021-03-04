ef4d4038899bb1ef5d91076b5674fda78f0f2023
kcin999 <62564441+kcin999@users.noreply.github.com>
2021-03-04 16:47:18 -0500

    Test Creation

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,16 +1,66 @@
 # -*- coding: utf-8 -*-
 import unittest
-
+import random
 from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_Fake(self):
+        items = [Item("fake", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEqual("fake", items[0].name)
+    
+    def test_Fake_Item_Quality_Standard(self):
+        orginalQuality = 10
+        sellInValue = 10
+        items = [Item("foo",sellInValue, orginalQuality)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].quality, orginalQuality-1)
+        self.assertEqual(items[0].sell_in, sellInValue-1)
 
+    def test_Fake_Item_Quality_PastSellIn(self):
+        orginalQuality = 10
+        sellInValue = -4
+        items = [Item("foo",sellInValue, orginalQuality)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].quality, orginalQuality-2)
+        self.assertEqual(items[0].sell_in, sellInValue-1)
+    
+    def test_Fake_Item_Quality_NegativeTest(self):
+        orginalQuality = 1
+        sellInValue = -1
+        items = [Item("foo",sellInValue, orginalQuality)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].quality, 0)
+        self.assertEqual(items[0].sell_in, sellInValue-1)
+
+    def test_AgedBrie_Quality(self):
+        orginalQuality = 15
+        items = [Item("Aged Brie",13,orginalQuality)]
+        gilded_rose=GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertTrue(items[0].quality == orginalQuality+1 or items[0].quality == 50)
+        self.assertEqual("Aged Brie", items[0].name)
         
+    def test_Sulfuras_Quality(self):
+        orginalQuality = 80
+        items = [Item("Sulfuras",13,orginalQuality)]
+        gilded_rose=GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].quality, orginalQuality)
+        self.assertEqual("Sulfuras", items[0].name)
+    
+    def test_BackStagePass_Quality_GreaterThan10DaysOut(self):
+        items = [Item("fake", 0, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual("fake", items[0].name)
+    
+
+
 if __name__ == '__main__':
     unittest.main()
--- python/texttest_fixture.py
+++ python/texttest_fixture.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
              Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
              Item(name="Aged Brie", sell_in=2, quality=0),
              Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
-             Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
-             Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+             Item(name="Sulfuras", sell_in=0, quality=80),
+             Item(name="Sulfuras", sell_in=-1, quality=80),
+             Item(name="Backstage passes", sell_in=15, quality=20),
+             Item(name="Backstage passes", sell_in=10, quality=49),
+             Item(name="Backstage passes", sell_in=5, quality=49),
              Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
             ]
 
