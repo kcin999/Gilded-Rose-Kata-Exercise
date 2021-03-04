f6137c6b2a7855e8d48d7da6eb5fd497a22beeb5
kcin999 <62564441+kcin999@users.noreply.github.com>
2021-03-04 18:55:20 -0500

    Python Refactor Complete Going to add Conjured

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -5,35 +5,43 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
+    def update_quality_aged_brie(self, item):
+        if item.quality < 49:
+            item.quality = item.quality +1
+        item.sell_in = item.sell_in - 1
+
+    def update_quality_backstage_passes(self, item):
+        if item.sell_in <= 0:
+            item.quality = 0
+        elif item.sell_in <= 5:
+            item.quality = item.quality + 3
+        elif item.sell_in <= 10:
+            item.quality = item.quality + 2
+        else:
+            item.quality = item.quality + 1
+
+        if item.quality > 50:
+            item.quality = 50
+        item.sell_in = item.sell_in - 1
+    
+    def update_quality_other_items(self, item):
+        if item.sell_in < 0:
+            item.quality = item.quality - 2
+        else:
+            item.quality = item.quality - 1
+        
+        if item.quality <0:
+            item.quality = 0
+        item.sell_in = item.sell_in- 1
+
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes":
-                if item.quality > 0:
-                    if item.name != "Sulfuras":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            if item.name == "Aged Brie":
+                self.update_quality_aged_brie(item)
+            elif item.name == "Backstage passes":
+                self.update_quality_backstage_passes(item)
+            elif item.name != "Sulfuras":
+                self.update_quality_other_items(item)
 
 
 class Item:
