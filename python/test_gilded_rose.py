74a315b79c12214c14b96e6b7a1061b928dab933
kcin999 <62564441+kcin999@users.noreply.github.com>
2021-03-04 17:21:56 -0500

    Test Handling Completed (No idea how to remove the repeats of the asserts). Moving on to the refactor before I add the conjor feature

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -5,62 +5,115 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_Fake(self):
-        items = [Item("fake", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual("fake", items[0].name)
-    
     def test_Fake_Item_Quality_Standard(self):
         orginalQuality = 10
         sellInValue = 10
-        items = [Item("foo",sellInValue, orginalQuality)]
+        name = "foo"
+        items = [Item(name,sellInValue, orginalQuality)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(items[0].quality, orginalQuality-1)
+        self.assertEqual(items[0].name, name)
         self.assertEqual(items[0].sell_in, sellInValue-1)
 
     def test_Fake_Item_Quality_PastSellIn(self):
         orginalQuality = 10
         sellInValue = -4
-        items = [Item("foo",sellInValue, orginalQuality)]
+        name = "foo"
+        items = [Item(name,sellInValue, orginalQuality)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(items[0].quality, orginalQuality-2)
+        self.assertEqual(items[0].name, name)
         self.assertEqual(items[0].sell_in, sellInValue-1)
     
     def test_Fake_Item_Quality_NegativeTest(self):
         orginalQuality = 1
         sellInValue = -1
-        items = [Item("foo",sellInValue, orginalQuality)]
+        name = "foo"
+        items = [Item(name,sellInValue, orginalQuality)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(items[0].quality, 0)
+        self.assertEqual(items[0].name, name)
         self.assertEqual(items[0].sell_in, sellInValue-1)
 
     def test_AgedBrie_Quality(self):
         orginalQuality = 15
-        items = [Item("Aged Brie",13,orginalQuality)]
+        sellInValue = 13
+        name = "Aged Brie"
+        items = [Item(name,sellInValue ,orginalQuality)]
         gilded_rose=GildedRose(items)
         gilded_rose.update_quality()
-        self.assertTrue(items[0].quality == orginalQuality+1 or items[0].quality == 50)
-        self.assertEqual("Aged Brie", items[0].name)
+        self.assertEqual(items[0].quality,orginalQuality+1)
+        self.assertEqual(name, items[0].name)
+        self.assertEqual(items[0].sell_in,sellInValue-1)
         
+    def test_AgedBrie_Quality_Over50(self):
+        orginalQuality = 50
+        sellInValue = 13
+        name = "Aged Brie"
+        items = [Item(name,sellInValue ,orginalQuality)]
+        gilded_rose=GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].quality,50)
+        self.assertEqual(name, items[0].name)
+        self.assertEqual(items[0].sell_in,sellInValue-1)
+
     def test_Sulfuras_Quality(self):
         orginalQuality = 80
-        items = [Item("Sulfuras",13,orginalQuality)]
+        sellInValue = 13
+        name = "Sulfuras"
+        items = [Item(name,sellInValue,orginalQuality)]
         gilded_rose=GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(items[0].quality, orginalQuality)
-        self.assertEqual("Sulfuras", items[0].name)
+        self.assertEqual(items[0].name, name)
+        self.assertEqual(items[0].sell_in,sellInValue)
     
     def test_BackStagePass_Quality_GreaterThan10DaysOut(self):
-        items = [Item("fake", 0, 0)]
+        orginalQuality = 20
+        sellInValue = 15 
+        name = "Backstage passes"
+        items = [Item(name, sellInValue, orginalQuality)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("fake", items[0].name)
-    
+        self.assertEqual(items[0].quality, orginalQuality+1)
+        self.assertEqual(items[0].name, name)
+        self.assertEqual(items[0].sell_in,sellInValue-1)
 
+    def test_BackStagePass_Quality_LessThan11DaysOut(self):
+        orginalQuality = 20
+        sellInValue = 10 
+        name = "Backstage passes"
+        items = [Item(name, sellInValue, orginalQuality)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].quality, orginalQuality+2)
+        self.assertEqual(items[0].name, name)
+        self.assertEqual(items[0].sell_in,sellInValue-1)
+    
+    def test_BackStagePass_Quality_LessThan6DaysOut(self):
+        orginalQuality = 20
+        sellInValue = 5 
+        name = "Backstage passes"
+        items = [Item(name, sellInValue, orginalQuality)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].quality, orginalQuality+3)
+        self.assertEqual(items[0].name, name)
+        self.assertEqual(items[0].sell_in,sellInValue-1)
+    
+    def test_BackStagePass_Quality_ConcertPast(self):
+        orginalQuality = 20
+        sellInValue = 0
+        name = "Backstage passes"
+        items = [Item(name, sellInValue, orginalQuality)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].quality, 0)
+        self.assertEqual(items[0].name, name)
+        self.assertEqual(items[0].sell_in,sellInValue-1)
 
 if __name__ == '__main__':
     unittest.main()
